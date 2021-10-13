5d20bd56acb2ceed1e56921c336c3ad5c2e16f1f
griffin-stewie <panterathefamilyguy@gmail.com>
2021-10-13 22:27:12 +0900

    bump version

--- Sources/mrkdwn/Commands/RootCommand.swift
+++ Sources/mrkdwn/Commands/RootCommand.swift
@@ -5,7 +5,7 @@ struct RootCommand: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "mrkdwn",
         abstract: "Support tool for writing Markdown documents.",
-        version: "1.0.0",
+        version: "1.1.0",
         subcommands: [GenTOFCommand.self]
     )
 }
